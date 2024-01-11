da55117a8c05a4425d4434d2e453231cb42d9a6e
md-azmi-siddique <md.azmi.siddique@gmail.com>
2024-01-11 14:32:17 +0600

    basic structure done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import List from './Components/List/List';
+import TaskList from './Components/TaskList';
 
 function App() {
   return (
     <>
-      <List />
+      {/* <List /> */}
+      <TaskList></TaskList>
     </>
   );
 }
